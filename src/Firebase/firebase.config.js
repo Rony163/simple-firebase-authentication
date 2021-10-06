34bfa3c7e063319c03107c15fea8eefd305d8306
Rony163 <minhajulislamrony43@gmail.com>
2021-10-06 15:17:38 +0600

    learning authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqNd2f9NgPqHv4s5WjeVFD_HYtmyP_nes",
+    authDomain: "simple-firebase-authenti-1c821.firebaseapp.com",
+    projectId: "simple-firebase-authenti-1c821",
+    storageBucket: "simple-firebase-authenti-1c821.appspot.com",
+    messagingSenderId: "312004383842",
+    appId: "1:312004383842:web:6441c368ea0e3f1e3c6d3b"
+};
+
+export default firebaseConfig;
