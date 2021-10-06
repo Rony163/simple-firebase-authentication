6bfc831bea2b74d9d49bc3c98dbf668079b5f9e6
Rony163 <minhajulislamrony43@gmail.com>
2021-10-06 15:58:55 +0600

    error handle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import './App.css';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import initializeAuthentication from './Firebase/firebase.initialize';
+import { useState } from 'react';
 
 initializeAuthentication();
 
 const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+
   const handleGoogleSignIn = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then(result => {
-        const user = result.user;
-        console.log(user);
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+        console.log(error.message);
       })
   }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
